70628b901e12cd6d6cc6b3606d2d13eb219c1612
anindita.basak@live.com <anindita.basak@live.com>
2024-05-05 11:20:44 +0530

    added az func script

---
+++ python_func/Az_function.sh
@@ -0,0 +1,10 @@
+#login to Azure account 
+az login
+# Create Azure Resource group 
+az group create --name glserverlessdemo-rg --location centralindia
+# Create Azure Storage Account 
+az storage account create --name glfuncdemostg --location centralindia --resource-group glserverlessdemo-rg --sku Standard_LRS
+# Create Azure Function App
+az functionapp create --resource-group glserverlessdemo-rg --consumption-plan-location westeurope --runtime python --runtime-version 3.10 --functions-version 4 --name httptriggergldemo --os-type linux --storage-account glfuncdemostg
+# Invoke the function App
+func azure functionapp publish httptriggergldemo
