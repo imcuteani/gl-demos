9d6b7811aa4cfbe7e431ba2a728c4285dd9742ff
anindita.basak@live.com <anindita.basak@live.com>
2024-05-05 16:17:50 +0530

    added ACR scripts

--- Azure_DevOps/acr.sh
+++ Azure_DevOps/acr.sh
@@ -1,10 +1,10 @@
 # Azure CLI automation script to deploy Azure Container Registry 
 
 # Create a resource group
-az group create --name gldevopsacr-rg --location centralindia
+az group create --name "<your_resource_group>" --location "<your_resource_group_location>"
 
 # Create a container registry
-az acr create --resource-group gldevopsacr-rg --name glacropsdemo --sku Basic
+az acr create --resource-group "<your_resource_group>" --name "<your_acr_demo>" --sku Basic
 
 # Delete Azure Resource Group 
-az group delete --name gldevopsacr-rg
+az group delete --name "<your_resource_group>"
