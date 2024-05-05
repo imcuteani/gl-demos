59831137e735f296cc85b26b064efac853f5c23d
anindita.basak@live.com <anindita.basak@live.com>
2024-05-05 16:16:34 +0530

    added Azure Container Registry scripts

--- Azure_DevOps/acr.sh
+++ Azure_DevOps/acr.sh
@@ -4,7 +4,7 @@
 az group create --name gldevopsacr-rg --location centralindia
 
 # Create a container registry
-az acr create --resource-group gldevopsacr-rg --name glacrdemo --sku Basic
+az acr create --resource-group gldevopsacr-rg --name glacropsdemo --sku Basic
 
 # Delete Azure Resource Group 
 az group delete --name gldevopsacr-rg
