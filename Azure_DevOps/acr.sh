41be7cf69d82f1a826e10944bb3c0308e35bccbe
anindita.basak@live.com <anindita.basak@live.com>
2024-05-05 11:50:24 +0530

    added Azure SQL DTU consumption

---
+++ Azure_DevOps/acr.sh
@@ -0,0 +1,10 @@
+# Azure CLI automation script to deploy Azure Container Registry 
+
+# Create a resource group
+az group create --name gldevopsacr-rg --location centralindia
+
+# Create a container registry
+az acr create --resource-group gldevopsacr-rg --name glacrdemo --sku Basic
+
+# Delete Azure Resource Group 
+az group delete --name gldevopsacr-rg
