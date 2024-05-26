501f45c3366eac91b393f5608f67697e89d5af63
anindita.basak@live.com <anindita.basak@live.com>
2024-05-26 12:27:45 +0530

    added VM deployment script

---
+++ Azure_VM/az_vm.sh
@@ -0,0 +1,23 @@
+# Create the Resorce Group 
+
+az group create --name $MY_RESOURCE_GROUP_NAME --location $REGION
+
+# Define env variables 
+
+export RANDOM_ID="$(openssl rand -hex 3)"
+export MY_RESOURCE_GROUP_NAME="myVMResourceGroup$RANDOM_ID"
+export REGION=EastUS
+export MY_VM_NAME="myVM$RANDOM_ID"
+export MY_USERNAME=azureuser
+export MY_VM_IMAGE="Canonical:0001-com-ubuntu-minimal-jammy:minimal-22_04-lts-gen2:latest"
+
+# Create Azure Linux VM 
+
+az vm create \
+    --resource-group $MY_RESOURCE_GROUP_NAME \
+    --name $MY_VM_NAME \
+    --image $MY_VM_IMAGE \
+    --admin-username $MY_USERNAME \
+    --assign-identity \
+    --generate-ssh-keys \
+    --public-ip-sku Standard
