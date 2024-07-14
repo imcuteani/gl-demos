a3b65283e961d2c51e41d54cc60b5f7a0ac747c8
anindita.basak@live.com <anindita.basak@live.com>
2024-07-14 16:37:51 +0530

    modified dtu consumption

--- python_func/Az_function.sh
+++ python_func/Az_function.sh
@@ -3,8 +3,10 @@ az login
 # Create Azure Resource group 
 az group create --name glserverlessdemo-rg --location centralindia
 # Create Azure Storage Account 
-az storage account create --name glfuncdemostg --location centralindia --resource-group glserverlessdemo-rg --sku Standard_LRS
+az storage account create --name glfuncdemostg --location centralindia --resource-group glserverlessdemo-rg --sku Standard_LRS 
 # Create Azure Function App
-az functionapp create --resource-group glserverlessdemo-rg --consumption-plan-location westeurope --runtime python --runtime-version 3.10 --functions-version 4 --name httptriggergldemo --os-type linux --storage-account glfuncdemostg
+az functionapp create --resource-group glserverlessdemo-rg --consumption-plan-location centralindia --runtime python --runtime-version 3.10 --functions-version 4 --name httptriggergldemo --os-type linux --storage-account glfuncdemostg
 # Invoke the function App
 func azure functionapp publish httptriggergldemo
+# Delete the Azure resource group 
+az group delete --name glserverlessdemo-rg
